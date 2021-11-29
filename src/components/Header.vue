a3b8675c88da96f647fec0edd5c8c588bc9c1b0c
MarianoPd <marianopidone@gmail.com>
2021-11-29 22:15:15 +0100

    inizio_select

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,38 @@
 <template>
-  <div >
+  <div class="d-flex justify-content-between">
     <img src="../assets/img/Spotify_logo.png" alt="icon">
+    <div>
+      <SelectGenre @genre="setGenre"/>
+    </div>
   </div>
 </template>
 
 <script>
+import SelectGenre from './SelectGenre.vue'
 export default {
     name: 'Header',
+    components:{
+      SelectGenre,
+    },
+    data(){
+      return{
+        genre: '',
+      }
+    },
+    methods:{
+      setGenre(text){
+        this.genre = text;
+         
+        this.$emit('genre', this.genre);
+        console.log(this.genre);
+      }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../assets/style/vars.scss';
+@import '../assets/style/generals.scss';
 @import '../assets/style/mixins.scss';
 div{
     height: 70px;
